b072b5213fa2889e18e76c6be80b41c70f298dcb
Gillian <jjjgillian@gmail.com>
2017-11-01 17:53:48 +0800

    10.Testing Class Name
    作者的習慣：在top level component share a class with class name
    .to.have.class  ::   https://github.com/chaijs/chai-jquery
    建議implementation first 再寫 spec

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,6 +2,11 @@ import { renderComponent , expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox Tests', ()=>{
+    it('has the corrent class', ()=>{
+        const component = renderComponent(CommentBox);
+        expect(component).to.have.class('comment-box');
+    }    );
+
     it('has a test area', () =>{
         const component = renderComponent(CommentBox);
         expect(component.find('textarea')).to.exist; // no() , chai will recongnize it  
@@ -9,7 +14,8 @@ describe('CommentBox Tests', ()=>{
     });
 
     it('has a button', ()=>{
-
+        const component = renderComponent(CommentBox);
+        expect(component.find('button')).to.exist;
     });
 
 });
