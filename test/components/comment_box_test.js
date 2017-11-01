00bfcb4f15f96e4df34621e66a163915b24040e7
Gillian <jjjgillian@gmail.com>
2017-11-01 18:01:03 +0800

    11.use beforeEach to condense the test
    beforeEach(); // run before each “it” statement
    
    Mocha run and excuses the file all of these function inside of the describes in the it are getting loaded or queued up to be executed;

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,19 +2,22 @@ import { renderComponent , expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox Tests', ()=>{
+    let component;
+     //run before each "it" 
+     beforeEach(()=>{
+        component = renderComponent(CommentBox);     
+    });
+
     it('has the corrent class', ()=>{
-        const component = renderComponent(CommentBox);
         expect(component).to.have.class('comment-box');
     }    );
 
     it('has a test area', () =>{
-        const component = renderComponent(CommentBox);
         expect(component.find('textarea')).to.exist; // no() , chai will recongnize it  
 
     });
 
     it('has a button', ()=>{
-        const component = renderComponent(CommentBox);
         expect(component.find('button')).to.exist;
     });
 
