1bb1aac5a8494ab2ec3c2652cac41a77278573ef
Gillian <jjjgillian@gmail.com>
2017-11-01 22:08:38 +0800

    16. Stub comment line
    We are going to build an action creator to save submitted comments and we’re also going to build a reducer that is going to keep application level state that consists of an array of all the comments submitted
    
    Use react-redux as helper connect to hook into our application level state (comment lists)

---
+++ src/components/comment_list.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+//no state -> functional component
+
+const CommentList = (props)=>{
+    return (
+        <ul className="comment-list"></ul>
+    );
+}
+
+export default CommentList;
---
+++ test/components/comment_list_test.js
@@ -0,0 +1,21 @@
+import {renderComponent, expect} from '../test_helper';
+import CommentList from '../../src/components/comment_list';
+
+describe('CommentList',()=>{
+    let component ;
+
+    beforeEach(()=>{
+            component = renderComponent(CommentList);
+        }
+    );
+
+
+    it('show comment with a li',()=>{
+        
+    });
+    
+    it('show comment list contents',()=>{
+
+    });
+
+});
