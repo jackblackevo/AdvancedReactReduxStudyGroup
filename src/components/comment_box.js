3ab182465f671f1ee729634f3486a296b021cfe4
Tse-Hsien Chiang <jackblackevo@gmail.com>
2017-11-02 18:05:49 +0800

    Add TextArea change event handler

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 
 export default class CommentBox extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      comment: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      comment: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="comment-box">
-        <textarea />
+        <textarea value={this.state.comment} onChange={this.handleChange} />
         <button>Submit comment</button>
       </div>
     )
