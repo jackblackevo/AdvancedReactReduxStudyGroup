4683c8bbfcd8d0c00d3005550f7e471aa6107562
akari0624 <akari0624@hotmail.com>
2017-11-08 22:23:27 +0800

    test Comment reducer

---
+++ test/reducer/comments_test.js
@@ -0,0 +1,25 @@
+import {expect}  from '../test_helper';
+import CommentReducer from '../../src/reducers/comments';
+import { SAVE_COMMENT} from '../../src/actions/types';
+
+
+
+describe('test CommentReducer',()=>{
+
+
+
+  it('handle action with unknow type',()=>{
+expect(CommentReducer(undefined,{})).to.be.instanceof(Array);
+expect(CommentReducer(undefined,{})).to.eql([]);
+
+  });
+
+  it('SAVE_COMMENT',()=>{
+
+    const action ={type:SAVE_COMMENT,payload:'new comment'}
+expect(CommentReducer([],action)).to.eql(['new comment']);
+
+  });
+
+
+});
