d5d912e16f801e4a830e5430d0788d3bba1af717
akari0624 <akari0624@hotmail.com>
2017-11-10 13:04:22 +0800

    use map index as key for comment LI

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 
 // functional component  沒有 render方法  直接return 要render的東西 , 也不會有自己的state , 完全純粹接收props 然後呈現畫面
 const CommentList = props => {
-  const list = props.comments.map(comment => <li key={comment}>{comment}</li>);
+  const list = props.comments.map((comment,index) => <li key={index}>{comment}</li>);
 
   return <ul className="comment-list">{list}</ul>;
 };
