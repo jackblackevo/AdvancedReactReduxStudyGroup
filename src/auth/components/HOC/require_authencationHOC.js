f70ae3550ace82f5d8786d70bb4f8b79518eba32
GillianY <jjjgrace01@gmail.com>
2017-11-29 11:01:21 +0800

    42 nesting HOC

--- src/auth/components/HOC/require_authencationHOC.js
+++ src/auth/components/HOC/require_authencationHOC.js
@@ -5,11 +5,10 @@ export default function(ComposedComponent){
 
     class Authencation extends Component{
 
-
-    static contextTypes ={
-      router:React.PropTypes.object
-    }
-
+        //  static contextTypes ={
+        //      router:React.PropTypes.object
+        //    }
+  
      componentWillMount(){
 
       console.log('props', this.props);
@@ -26,6 +25,8 @@ export default function(ComposedComponent){
            }
 
       render(){
+          //console.log(this.props.isLoggedIn);
+          //console.log(this.context);
           return <ComposedComponent {...this.props} />;
       }
     }
